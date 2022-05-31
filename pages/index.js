a0ad9937969cb964bd71a97eedb2d84743fadca3
eds2002 <es23jr@gmail.com>
2022-05-30 20:33:27 -0400

    updates

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home({featured}) {
 }
 
 export async function getServerSideProps({req,res}) {
-    const blogRes = await fetch(`https://dashboard.heroku.com/blog/38`)
+    const blogRes = await fetch(`https://dashboard.heroku.com/blog/44`)
     const blogData = await blogRes.json()
     return{props: {featured: blogData.data}}
 }
